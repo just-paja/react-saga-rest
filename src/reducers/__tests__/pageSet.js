7afd3dfd74fd64fbd60c0641bbea718f890c8701
Pavel Žák <pavel@berrycloud.cz>
2018-04-17 11:40:00 +0200

    Support payload in pageSet

--- src/reducers/__tests__/pageSet.js
+++ src/reducers/__tests__/pageSet.js
@@ -17,6 +17,22 @@ describe('pageSet reducer', () => {
     });
   });
 
+  it('returns invalidated state with new page passed as payload', () => {
+    const state = {
+      valid: true,
+      userId: 15,
+      page: 10,
+    };
+    const result = reducers.pageSet(state, {
+      payload: 1,
+    });
+    expect(result).not.toBe(state);
+    expect(result).toMatchObject({
+      valid: false,
+      page: 1,
+    });
+  });
+
   it('returns same state when page did not change', () => {
     const state = {
       valid: true,
--- src/reducers/pageSet.js
+++ src/reducers/pageSet.js
@@ -3,8 +3,11 @@ import {
   STATE_PAGE,
 } from '../constants';
 
-export default (state, action) => (state[STATE_PAGE] === action[STATE_PAGE] ? state : ({
-  ...state,
-  [FLAG_VALID]: false,
-  [STATE_PAGE]: action[STATE_PAGE],
-}));
+export default (state, action) => {
+  const actionPage = typeof action[STATE_PAGE] === 'undefined' ? action.payload : action[STATE_PAGE];
+  return (state[STATE_PAGE] === actionPage ? state : ({
+    ...state,
+    [FLAG_VALID]: false,
+    [STATE_PAGE]: actionPage,
+  }));
+};
